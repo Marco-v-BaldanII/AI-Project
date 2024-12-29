928bebe001b8161f3c0b30d3e8c231da7f7ce405
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2024-12-29 20:46:34 +0100

    bulborb can be carried back

--- Assets/FinalProject/Scripts/Enemies/Bulborb.cs
+++ Assets/FinalProject/Scripts/Enemies/Bulborb.cs
@@ -9,10 +9,15 @@ public class Bulborb : MonoBehaviour
 	public Image img;
 	private  float hp;
 	public float maxHP;
+	private Animator animator;
+	public Component[] components_to_disable;
+	private GrabbableObject grab_object;
 	
 	private void Awake()
 	{
 		hp = maxHP;
+		grab_object = GetComponent<GrabbableObject>();
+		animator = GetComponent<Animator>();
 	}
 
 	public void OnTriggerEnter(Collider other)
@@ -20,13 +25,31 @@ public class Bulborb : MonoBehaviour
         if (other.tag == "PikminAttack")
         {
 	        hp--;
+	        animator.SetFloat("hp", hp);
 	        print("Enemy hurt, hp is now " + hp);
 	        
 	        img.fillAmount = (1f / maxHP) * hp;
 	        
 	        if (hp <= 0)
 	        {
-	        	Destroy(this.gameObject);
+		        grab_object.enabled = true;
+		        
+		        foreach (Component component in components_to_disable)
+		        {
+		        	Destroy(component);
+		        }
+		        
+		        Collider[] colliders = GetComponentsInChildren<Collider>();
+		        
+		        foreach (Collider col in colliders)
+		        {
+		         if(col.gameObject != this.gameObject)	{Destroy(col);}
+		        }
+		        
+		        // Change tag so can be grabbed
+		        this.gameObject.tag = "Pellet";
+		        
+		        Destroy(this);
 	        }
             
         }
