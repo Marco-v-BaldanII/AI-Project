d346ffb3a4ede65c327b552820cacb04e264ae2a
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2024-12-29 21:11:20 +0100

    weight display

---
+++ Assets/FinalProject/Scripts/SeparatedUI/TextDisplays/WeightIcon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class WeightIcon : MonoBehaviour
+{
+	public TextMeshProUGUI text;
+	private GrabbableObject grab_object;
+	
+    // Start is called before the first frame update
+	void Awake()
+    {
+	    text = GetComponent<TextMeshProUGUI>();
+	    grab_object = GetComponentInParent<GrabbableObject>();
+	    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		text.text = grab_object.num_pikmin.ToString() + "\n" + "_\n\n\n" + grab_object.weight.ToString();
+    }
+}
